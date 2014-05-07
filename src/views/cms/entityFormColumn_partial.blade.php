c97777d1352ad8d9e5109ff02403e28de2b588b4
Philippe Lonchampt <philippelonchampt@gmail.com>
2014-05-07 15:53:41 +0200

    add col-md-X for non-list fields

--- src/views/cms/entityFormColumn_partial.blade.php
+++ src/views/cms/entityFormColumn_partial.blade.php
@@ -1,6 +1,6 @@
 @foreach($col as $key)
 
-    <div class="form-group sharp-field-{{ $col->$key->type }} {{ $errors->first($key) ? 'has-error' : '' }}">
+    <div class="form-group sharp-field-{{ $col->$key->type }} {{ $errors->first($key) ? 'has-error' : '' }} col-md-{{ $col->$key->field_width ?: '12' }}">
         {{ \Dvlpp\Sharp\Form\Facades\SharpCmsField::make($key, $col->$key, $instance) }}
         <p class="help-block">
             {{ $errors->first($key) ?: $col->$key->help }}
