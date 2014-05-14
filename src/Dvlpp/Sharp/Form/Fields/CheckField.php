1e54e0fddad2c2b94011d3a3569d415002b964fd
Philippe Lonchampt <philippelonchampt@gmail.com>
2014-05-14 12:18:53 +0200

    Update checkfield to send it checked or not

--- src/Dvlpp/Sharp/Form/Fields/CheckField.php
+++ src/Dvlpp/Sharp/Form/Fields/CheckField.php
@@ -7,7 +7,8 @@ class CheckField extends AbstractSharpField {
     function make()
     {
         $str = '<div class="checkbox"><label>';
-        $str .= Form::checkbox($this->fieldName, $this->fieldValue, $this->field->checked);
+        $str .= Form::hidden($this->fieldName, 0);
+        $str .= Form::checkbox($this->fieldName, 1, $this->field->checked);
         $str .= $this->field->text . '</label></div>';
         return $str;
     }
