d4bd08c4cc39f9aa54b68744f605bf1ffd9e6e14
Philippe Lonchampt <philippelonchampt@gmail.com>
2014-07-24 16:11:57 +0200

    Fix checkfield unchecked value

--- src/Dvlpp/Sharp/Form/Fields/CheckField.php
+++ src/Dvlpp/Sharp/Form/Fields/CheckField.php
@@ -6,10 +6,12 @@ class CheckField extends AbstractSharpField {
 
     function make()
     {
-        $str = '<div class="checkbox"><label>';
+        // Put an hidden field with same name and 0 value in order to send it
+        // in case of unchecked checkbox. Browser will choose the latest field.
+        $str = Form::hidden($this->fieldName, 0);
+        $str .= '<div class="checkbox"><label>';
         $str .= Form::checkbox($this->fieldName, 1, $this->field->checked);
         $str .= $this->field->text . '</label></div>';
-        $str .= Form::hidden($this->fieldName, 0);
         return $str;
     }
 
