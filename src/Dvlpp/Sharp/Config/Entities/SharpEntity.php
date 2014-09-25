4930f19f1ed54e469e28538689b24cbe56e4fd48
Philippe Lonchampt <philippelonchampt@gmail.com>
2014-09-25 10:08:45 +0200

    Add entity config extend feature

--- src/Dvlpp/Sharp/Config/Entities/SharpEntity.php
+++ src/Dvlpp/Sharp/Config/Entities/SharpEntity.php
@@ -1,6 +1,9 @@
 <?php namespace Dvlpp\Sharp\Config\Entities;
 
 
+use Dvlpp\Sharp\Config\SharpCmsConfig;
+use Dvlpp\Sharp\Exceptions\EntityConfigurationNotFoundException;
+
 class SharpEntity extends HasProperties {
 
     protected $mandatoryProperties = ["label", "repository"];
@@ -16,6 +19,31 @@ class SharpEntity extends HasProperties {
         "form_fields" => 'Dvlpp\Sharp\Config\Entities\SharpEntityFormFields',
         "form_layout" => 'Dvlpp\Sharp\Config\Entities\SharpEntityFormLayout'
     ];
+
+
+    public function __construct($data, $parent)
+    {
+        parent::__construct($data, $parent);
+
+        if(isset($this->data['extends']) && $this->data['extends'])
+        {
+            // This Entity is configured to extend another entity config
+            list($cat, $ent) = explode(".", $this->data['extends']);
+            $extendedEntity = SharpCmsConfig::findEntity($cat, $ent);
+            if(!$extendedEntity)
+            {
+                throw new EntityConfigurationNotFoundException("Extended entity [".$this->data['extends']."] configuration not found");
+            }
+
+            $entendedEntityData = $extendedEntity->getData();
+            $this->data = array_merge($entendedEntityData, $this->data);
+        }
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
 
 }
 
