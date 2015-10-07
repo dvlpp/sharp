621103c5f28835c80ff90be4b28e1ebed69672c3
Philippe Lonchampt <philippelonchampt@gmail.com>
2015-10-07 11:24:01 +0200

    Return an array

--- src/Dvlpp/Sharp/Repositories/AutoUpdater/SharpUploadUpdaterTrait.php
+++ src/Dvlpp/Sharp/Repositories/AutoUpdater/SharpUploadUpdaterTrait.php
@@ -5,12 +5,19 @@ namespace Dvlpp\Sharp\Repositories\AutoUpdater;
 trait SharpUploadUpdaterTrait
 {
 
+    /**
+     * Move the file and return a ["path", "mime", "size"] array.
+     *
+     * @param $file
+     * @param $relativeDestDir
+     * @return array|null
+     */
     protected function moveUploadedFile($file, $relativeDestDir)
     {
         $storageDisk = config("sharp.upload_storage_disk") ?: "local";
 
         if (starts_with($file, ":DUPL:")) {
-            // Duplication case
+            // Duplication case: file in on the storage disk
             $duplication = true;
             $file = substr($file, strlen(":DUPL:"));
             $relativeSrcFile = config("sharp.upload_storage_base_path") . "/$file";
@@ -30,20 +37,33 @@ trait SharpUploadUpdaterTrait
             // Prepend prefix to destdir (from sharp config)
             $relativeDestDir = config("sharp.upload_storage_base_path") . "/$relativeDestDir";
 
+            // Create storage dir if needed
             if (!\Storage::disk($storageDisk)->exists($relativeDestDir)) {
                 \Storage::disk($storageDisk)->makeDirectory($relativeDestDir, 0777, true);
             }
 
+            // Find an available name for the file
             $fileName = $this->findAvailableFileName($relativeDestDir, $fileName, $storageDisk);
 
-            \Storage::disk($storageDisk)
-                ->put("$relativeDestDir/$fileName", \Storage::disk($srcFileDisk)->get($relativeSrcFile));
+            // And finally, copy
+            \Storage::disk($storageDisk)->put(
+                "$relativeDestDir/$fileName",
+                \Storage::disk($srcFileDisk)->get($relativeSrcFile));
+
+            // Get mime and size from the $srcFileDisk: if the storage is in cloud,
+            // it will be faster this way.
+            $mime = \Storage::disk($srcFileDisk)->mimeType($relativeSrcFile);
+            $size = \Storage::disk($srcFileDisk)->size($relativeSrcFile);
 
             if(!$duplication) {
                 \Storage::disk($srcFileDisk)->delete($relativeSrcFile);
             }
 
-            return "$relativeDestDir/$fileName";
+            return [
+                "path" => "$relativeDestDir/$fileName",
+                "mime" => $mime,
+                "size" => $size
+            ];
         }
 
         return null;
