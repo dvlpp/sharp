735abed1eea033f18e03f4109be784a1ae017281
Philippe Lonchampt <philippelonchampt@gmail.com>
2015-10-27 16:22:54 +0100

    File valuator : fix named queue (stupid)

--- src/Dvlpp/Sharp/Repositories/AutoUpdater/Valuators/FileValuator.php
+++ src/Dvlpp/Sharp/Repositories/AutoUpdater/Valuators/FileValuator.php
@@ -152,8 +152,14 @@ class FileValuator implements Valuator
             // And finally, copy. We do this in a Job to authorize queue
             // on configured environments
             $this->dispatch(
-                new CopyFileInStorage($relativeSrcFile, "$relativeDestDir/$fileName", $srcFileDisk, $storageDisk, !$duplication)
-            )->onQueue("sharp-files");
+                (new CopyFileInStorage(
+                    $relativeSrcFile,
+                    "$relativeDestDir/$fileName",
+                    $srcFileDisk,
+                    $storageDisk,
+                    !$duplication)
+                )->onQueue("sharp-files")
+            );
 
             return [
                 "path" => "$relativeDestDir/$fileName",
